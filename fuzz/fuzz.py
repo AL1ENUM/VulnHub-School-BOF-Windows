7e31a1040e972f61a6a49f7da2b2306e3e51e09d
alienum <67610662+AL1ENUM@users.noreply.github.com>
2020-12-14 00:04:35 +0200

    Update fuzz.py

--- fuzz/fuzz.py
+++ fuzz/fuzz.py
@@ -1,7 +1,7 @@
 import sys, socket
 import time
 
-buffer = "A" * 100
+buffer = "A" * 500
 
 while True:
 
@@ -9,12 +9,12 @@ while True:
      s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
      s.connect(('127.0.0.1',23))
      s.recv(1024)
-     buffer = buffer + "A" * 100
+     buffer = buffer + "A" * 500
      s.send((buffer))
      s.close()
      time.sleep(1)
 
  except:
-        #Fuzzing crashed at 1900 bytes
+        #
         print("Fuzzing crashed at %s bytes" % str(len(buffer)))
         sys.exit()
